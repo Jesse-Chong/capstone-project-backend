45cacd8886633101e9e4c31fa183674d2395e223
jameel914 <jameelibrahim107@gmail.com>
2024-02-29 13:55:28 -0500

    deleted unwated data on seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,12 +1,4 @@
 \c maps_places;
-  user_id SERIAL PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  user_name VARCHAR(50) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  password_hash VARCHAR(255) NOT NULL,
-  preferred_language TEXT,
-  documents TEXT
 
 INSERT INTO users (user_id, first_name, last_name, user_name, email, password_hash, preferred_language, documents)
 VALUES
